1500c74821ee22cd471a8b6d6cd65e16f849f56b
MoriyaBarel <93195727+MoriyaBarel@users.noreply.github.com>
2022-03-09 12:46:42 +0200

    Add files via upload

---
+++ mat.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdexcept>
+#include "mat.hpp"
+
+using namespace ariel;
+
+    string ariel::mat(int col, int row, char c1, char c2){
+        return "h";
+    }
+
+        
---
+++ mat.hpp
@@ -0,0 +1,6 @@
+#include <string>
+using namespace std;
+
+namespace ariel {
+    string mat(int col, int row, char c1, char c2);
+}
